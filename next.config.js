f26663082f06b8ac240d8fca3825165a01b3fda8
Arga Roh Sahrijal Saragih <arga.saragih@gmail.com>
2020-11-06 20:41:22 +0700

    Precache all routes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 const withPWA = require("next-pwa");
+const slugify = require("slugify");
+
+const slugOptions = {
+  lower: true,
+};
+const songs = require("./songs.json");
+
+const additionalEntries = [
+  ...songs.map(song => ({ url: `/nomor/${song.number}.html`, revision: null })),
+  ...songs.map(song => ({ url: `/judul/${slugify(song.title.split(". ")[1], slugOptions)}.html`, revision: null})),
+];
 
 module.exports = withPWA({
   // Target must be serverless
@@ -6,5 +17,6 @@ module.exports = withPWA({
   pwa: {
     disable: process.env.NODE_ENV === "development",
     dest: "public",
+    additionalManifestEntries: additionalEntries
   },
 });
