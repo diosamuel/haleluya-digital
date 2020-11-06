a384932435fda06a54212f19da8bc42b0b9e74cb
Arga Roh Sahrijal Saragih <arga.saragih@gmail.com>
2020-11-07 00:53:15 +0700

    Change Bagikon to Bagihon

--- pages/components/ShareButton.js
+++ pages/components/ShareButton.js
@@ -24,7 +24,7 @@ export default function ShareButton({ title = "", lyrics = "" }) {
         style={{ fontSize: "1.5rem", marginBottom: "1rem" }}
         onClick={handleShareClick}
       >
-        Bagikon
+        Bagihon
       </button>
     );
   } else {
