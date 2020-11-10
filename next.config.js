c326b88e5e4a186855c92dd7b19369edc9ce3c2f
Arga Roh Sahrijal Saragih <arga.saragih@gmail.com>
2020-11-10 20:30:27 +0700

    Precache all pages JSON data

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const withPWA = require("next-pwa");
 const slugify = require("slugify");
+const buildId = Math.random() * 1_000_000_000;
 
 const slugOptions = {
   lower: true,
@@ -9,11 +10,16 @@ const songs = require("./songs.json");
 const additionalEntries = [
   ...songs.map(song => ({ url: `/nomor/${song.number}.html`, revision: null })),
   ...songs.map(song => ({ url: `/judul/${slugify(song.title.split(". ")[1], slugOptions)}.html`, revision: null})),
+  ...songs.map(song => ({ url: `_next/data/${buildId}/nomor/${song.number}.json`, revision: null})),
+  ...songs.map(song => ({ url: `_next/data/${buildId}/judul/${slugify(song.title.split(". ")[1], slugOptions)}.json`, revision: null})),
 ];
 
 module.exports = withPWA({
   // Target must be serverless
   target: "serverless",
+  generateBuildId: () => {
+    return String(buildId);
+  },
   pwa: {
     disable: process.env.NODE_ENV === "development",
     dest: "public",
