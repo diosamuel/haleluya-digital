76b1d1fc8f6ed2f533eab94aa613caaab7e74c08
Arga Roh Sahrijal Saragih <arga.saragih@gmail.com>
2021-12-07 02:22:09 +0700

    Remove unused gtag helpers

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import { useEffect } from "react";
 import Router, { useRouter } from "next/router";
 import NProgress from "nprogress";
 import "nprogress/nprogress.css";
-import * as gtag from "../lib/gtag";
 import "../styles/globals.css";
 import "react-awesome-button/dist/styles.css";
 import "react-awesome-button/dist/themes/theme-blue.css";
@@ -17,9 +16,6 @@ function MyApp({ Component, pageProps }) {
   useEffect(() => {
     const handleRouteChange = (url) => {
       window.scrollTo({ top: 0 });
-      if (process.env.NODE_ENV === "production") {
-        gtag.pageview(url);
-      }
     };
     router.events.on("routeChangeComplete", handleRouteChange);
     return () => {
